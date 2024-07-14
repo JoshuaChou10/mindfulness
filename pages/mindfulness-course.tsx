264b1df54b75798e48db486bac4a1029262c43b6
Joshua Chou <joshuachou912@gmail.com>
2024-07-14 09:05:31 +0800

    Finished excercise for first section

--- pages/mindfulness-course.tsx
+++ pages/mindfulness-course.tsx
@@ -11,18 +11,24 @@ interface Section {
 const sections: { [key: string]: Section } = {
   "Day One: Mindfulness": {
     practice: `
-      <h2>Something else here</h2>
-      <p> Hello and welcome to the world of mindfulness. This course is intended for anyone looking to integrate mindfulness into their everyday lives, living every moment in peace, harmony and clarity. </p>
-<p>To put it simply, mindfulness means living in the Now. It means being aware of the thoughts and feelings that constantly run underground our attention. Are you aware of what is going on now? That is what matters. </p>
+    <h2>The Senses</h2>
+    <p>This is a good and simple 5-10 minute exercise that will help strengthen your mindfulness. I remember this was my favorite exercise when I first started. I first learned this from Oli Doyle’s book, Mindfulness Plain and Simple. </p>
+    <p>Sit down and take in two to three deep breaths to quiet down. Now bring your attention to the sounds around you. Listen, what do you hear? The sounds of traffic, the birds chirping, people talking. Any sounds, far or near, take it all in. You may notice an urge to label, or judge the sounds, but we are simply listening. It is just sound, listen closely and you will get to be aware of many small things that you weren’t aware of before.  </p>
+    <p>Now let’s bring our attention to any feelings on the body. Any tension or stress, just notice each one and relax. Be aware of any contact or pressure. For example, you can be aware of how it feels to sit, the feeling of the feet on the ground. Anything you can find, from head to toe, let your awareness sweep through every part. </p>
+    <p>Slowly stand up, and notice how that feels. The legs stretching, the pressure shifting. Open your eyes and take in all the vibrant colors around you. Notice each shape. Notice the different shades of color. Notice any movement.</p>
+    <p>Take it all in, any sounds, sensations, sights. Whatever you hear, feel, or see let your awareness feel all of it. </p>
+    
 
     `,
     text: `
       <h1>An Introduction</h1>
-      <p> Hello and welcome to the world of mindfulness. Here you will find a practical guide to living every moment fuller and clearer, connected with the now. When we are living mindfully every moment is enjoyed with clarity, peace and a sense of gratitude. </p>
-      <p>To put it simply, mindfulness means living in the present moment. It means not being lost thinking of the past or future, but instead being aware of the thoughts that arise here, moment to moment. When we start to become aware of the thoughts and feelings that constantly run underground our attention we begin to notice many things weren’t aware of before.  Are you aware of what is going on now? That’s what matters. </p> 
- <p>Many of us live through our thoughts, never truly experiencing things as they are. It can be a while before we realize that freedom is in the present moment, not in some past or future. When living mindfully we get to truly experience things as they are. When walking in a trail, you enjoy the soft breeze on your face, you enjoy the bright yellow sunlight shining down, and the crunch of the leaves beneath your feet. You enjoy the feeling of your foot contacting the ground as you walk, and all the vibrant colors around you. </p> 
- <p>This course is structed with a new aspect of mindfulness to practice everyday. Each section includes an applicble way to bring mindfulness and clarity into our lives. My hope is that with this course as a guide, you will be able to live every moment connected with the now, with a sense of lightness, peace and harmony with what is. </p>
- <p> So let’s start with today. Whenever you remember bring your attention back to the Now. If you notice you are lost in thought, worrying about something in the future, or lost somewhere in the past, then bring your attention back to the present. Below there is an exercise where you can use your five senses to stay connected with the here and now.</p>
+      <p> Hello and welcome to the world of mindfulness. Here you will find a practical guide to living every moment fuller and clearer, connected with the present moment. When we are living mindfully every moment is enjoyed with clarity, peace, and a sense of gratitude. </p>
+      <p>To put it simply, mindfulness means living in the here and now. It means not being lost thinking of the past or future, but instead, being aware of the thoughts that arise here, moment to moment. When we start to become aware of the thoughts and feelings that constantly run underground our attention we begin to notice many things we weren’t aware of before.  Are you aware of what is going on now? That’s what matters. </p> 
+ <p>Many of us have never truly lived. We experience our lives only through the dull screen of our thoughts. It can be a while before we realize that freedom is found in the present moment, not in some past or future up there in our head. When living mindfully we get to truly experience things as they are. When walking in a trail, you enjoy the soft breeze on your face, the warmth of the bright yellow sunlight shining down, and the crunch of the leaves beneath your feet. The crisp birdsongs, the feeling of your foot contacting the ground as you walk, the soft sensation of the breath. This is mindfulness. </p> 
+ <p>This course is structured with a new way to get connected with ourselves every day. Each section includes an applicable way to bring mindfulness and clarity into our lives. My hope is that with this course as a guide, you will be able to live every moment connected with the now, with a sense of lightness, peace, and harmony with what is. </p>
+ <p> So let’s start with today. If you pay attention you will find that most of us are constantly lost in thought. Lost in entrancing stories of the past or the future. We miss what truly matters, right here under our nose. So as often as you can, bring your attention back to the Now. If you notice you are lost in thought, worrying about something in the future, or lost somewhere in the past, then bring your attention back to the present. Then in that moment, you are free. Free from all the stories that we get lost in, and back in reality. Back in the here and now. 
+ <p> Below there is an exercise where you can use your five senses to stay connected. </p>
+ 
  
  
   `,
